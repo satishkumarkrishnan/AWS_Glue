c1563262ca5a29173307da7cdf43d8cecf16fe27
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-24 16:37:11 +0530

    AWS Glue catalogue and crawler

--- s3.tf
+++ s3.tf
@@ -1,4 +1,4 @@
-resource "aws_s3_bucket" "rawdatabucket" {
+/*resource "aws_s3_bucket" "rawdatabucket" {
   bucket = "raw-data-bucket"  
 # Prevent accidental deletion of this S3 bucket
   lifecycle {
@@ -8,4 +8,40 @@ resource "aws_s3_bucket" "rawdatabucket" {
 
 resource "aws_s3_bucket" "extensionbucket" {
   bucket = "extension-data-bucket"
+}*/
+
+# S3 bucket to store App
+resource "aws_s3_bucket" "example1" {
+  bucket = "tokyo-rawdata-bucket"
+  # Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+# Enable versioning so you can see the full revision history of your
+# state files
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.example1.id
+    versioning_configuration {
+    status = "Enabled"
+  }
+}
+# Enable server-side encryption by default
+resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
+  bucket = aws_s3_bucket.example1.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+# Explicitly block all public access to the S3 bucket
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.example1.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
