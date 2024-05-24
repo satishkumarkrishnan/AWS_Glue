fb7d03bf0b4a6e42abae346dab7eea0819856bd0
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-24 16:24:59 +0530

    AWS Glue catalogue and crawler with S3 config

--- s3.tf
+++ s3.tf
@@ -1,5 +1,9 @@
 resource "aws_s3_bucket" "bucket1" {
   bucket = "raw-data-bucket"  
+# Prevent accidental deletion of this S3 bucket
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
 resource "aws_s3_bucket" "bucket2" {
