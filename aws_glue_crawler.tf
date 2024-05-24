87d71993353e815b15b961d49b81eb0b6d1b811f
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-24 16:12:10 +0530

    AWS Glue catalogue and crawler with S3 config

--- aws_glue_crawler.tf
+++ aws_glue_crawler.tf
@@ -13,6 +13,6 @@ resource "aws_glue_crawler" "example" {
   role          = aws_iam_role.gluerole.arn
 
   s3_target {
-    path = "s3://${aws_s3_bucket.bucket2}"
+    path = "s3://${aws_s3_bucket.bucket1.bucket}"
   }  
 }
