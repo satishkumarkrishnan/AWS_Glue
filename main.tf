db4b945a2984fdb3726b26eebe6ccce61253abdb
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-24 13:10:56 +0530

    VPC for AWS Glue provisioning

--- main.tf
+++ main.tf
@@ -13,3 +13,18 @@ terraform {
 module "aws_glue" {
   source    = "git@github.com:satishkumarkrishnan/terraform-aws-vpc.git?ref=main"   
 }
+
+#To use AWS Glue config
+resource "aws_glue_job" "tokyo_aws_glue" {
+  name = "jsontocsv"
+  role_arn = aws_iam_role.gluerole.arn
+
+  command {
+    script_location = "s3://${aws_s3_bucket.bucket.bucket}/awsgluescript.py"
+    python_version = 3
+  }
+
+  provisioner "local-exec" {
+    command = "aws glue start-job-run --job-name ${aws_glue_job.jsontocsv.name} --region ${var.region}"
+  }
+}
