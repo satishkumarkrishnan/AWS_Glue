d3fffef823874404f5446570c855a827b09b5b29
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-24 13:12:31 +0530

    VPC for AWS Glue provisioning

---
+++ aws_glue_crawler.tf
@@ -0,0 +1,20 @@
+resource "aws_glue_crawler" "crawler" {
+  database_name = aws_glue_catalog_database.db.name
+  name = "fruitcrawler"
+  role = aws_iam_role.gluerole.arn
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.bucket.bucket}"
+  }
+
+ provisioner "local-exec" {
+    command = "aws glue start-crawler --name ${aws_glue_crawler.crawler.name} --region ${var.region}"
+  }
+}
+
+resource "null_resource" "run_crawler" {
+  # Changes to the crawler's S3 path requires re-running
+  triggers = {
+    s3_path = "${aws_glue_crawler.crawler.s3_target.0.path}"
+  }
+}
