491ff054cdace4b3817543e3007f8ad9dddd063f
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-24 16:14:40 +0530

    AWS Glue catalogue and crawler with S3 config

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "bucket1" {
-  bucket = "raw_data_bucket"  
+  bucket = "raw-data-bucket"  
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "extension_data_bucket"
+  bucket = "extension-data-bucket"
 }
