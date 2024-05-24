8793a417771f76225a587a3eefe7e5bbd74eaf7a
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-24 15:53:37 +0530

    AWS Glue catalogue and crawler

--- aws_glue_crawler.tf
+++ aws_glue_crawler.tf
@@ -1,20 +1,18 @@
-resource "aws_glue_crawler" "crawler" {
-  database_name = aws_glue_catalog_database.db.name
-  name = "fruitcrawler"
-  role = aws_iam_role.gluerole.arn
+/*resource "aws_glue_crawler" "tokyo_crawler" {
+  database_name = var.database_name
+  description   = var.glue_crawler_description
+  role          = var.role  
+
+
+}*/
+
+resource "aws_glue_crawler" "example" {
+  #database_name = "${aws_glue_catalog_database.example.name}"
+  database_name = var.database_name
+  name          = "tokyo_crawler"
+  role          = aws_iam_role.gluerole.arn
 
   s3_target {
-    path = "s3://${aws_s3_bucket.bucket.bucket}"
-  }
-
- provisioner "local-exec" {
-    command = "aws glue start-crawler --name ${aws_glue_crawler.crawler.name} --region ${var.region}"
-  }
-}
-
-resource "null_resource" "run_crawler" {
-  # Changes to the crawler's S3 path requires re-running
-  triggers = {
-    s3_path = "${aws_glue_crawler.crawler.s3_target.0.path}"
-  }
+    path = "s3://${aws_s3_bucket.example.bucket}"
+  }  
 }
---
+++ iam.tf
@@ -0,0 +1,15 @@
+resource "aws_iam_role" "gluerole" {
+  name               = "gluerole"
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Effect": "Allow",
+        "Principal": {
+          "Service": "glue.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+      }
+    ]
+  })
+}
