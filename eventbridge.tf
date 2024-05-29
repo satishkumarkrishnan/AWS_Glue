ad8b5ca01beeb624df50128c831c0da651eb1f99
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-29 14:38:29 +0530

    Adding AWS Eventbridge and S3 notification

---
+++ eventbridge.tf
@@ -0,0 +1,16 @@
+resource "aws_cloudwatch_event_rule" "event_from_s3" {
+  name = "s3_put_object_event"
+  description = "Capture s3 object creation event"
+
+  event_pattern = jsonencode(
+    {
+        "source": ["aws.s3"],
+        "detail-type": ["Object Created"],
+        "detail":{
+            "bucket": {
+                "name": ["${aws_s3_bucket.source_bucket.id}"]
+            }
+        }
+    }
+  )
+}
--- s3.tf
+++ s3.tf
@@ -49,3 +49,8 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket      = aws_s3_bucket.example1.id
+  eventbridge = true
+}
