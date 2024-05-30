52e856787f8070f7134fb4a30a544692f342bc25
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-30 17:30:31 +0530

    Adding AWS cloud Trail resource to log S3 events

--- s3.tf
+++ s3.tf
@@ -75,7 +75,7 @@ resource "aws_s3_bucket_policy" "logs" {
 resource "aws_kms_key" "cloudtrail-logs-kms-key" {
   key_usage           = "ENCRYPT_DECRYPT"
   enable_key_rotation = false
-  policy              = templatefile("${path.module}/cloudtrail-logs-kms-key.json",{ account_id = data.aws_caller_identity.current.account_id })
+  policy              = templatefile("${path.module}/cloudtrail-logs-kms-key.json",590183849298)
 }
 
 resource "aws_kms_alias" "kms-alias-logs" {
