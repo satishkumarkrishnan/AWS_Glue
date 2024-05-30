312c125718f1bb13af6bc038330dc90330cd591f
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-30 13:02:45 +0530

    Adding AWS SNS Topic to send notification

--- s3.tf
+++ s3.tf
@@ -1,14 +1,22 @@
-/*resource "aws_s3_bucket" "rawdatabucket" {
-  bucket = "raw-data-bucket"  
-# Prevent accidental deletion of this S3 bucket
-  lifecycle {
-    prevent_destroy = false
-  }
-}
+#To Create SNS Topic 
+resource "aws_sns_topic" "topic" {
+  name = "s3-event-notification-topic"
 
-resource "aws_s3_bucket" "extensionbucket" {
-  bucket = "extension-data-bucket"
-}*/
+  policy = <<POLICY
+{
+    "Version":"2012-10-17",
+    "Statement":[{
+        "Effect": "Allow",
+        "Principal": { "Service": "s3.amazonaws.com" },
+        "Action": "SNS:Publish",
+        "Resource": "arn:aws:sns:*:*:s3-event-notification-topic",
+        "Condition":{
+            "ArnLike":{"aws:SourceArn":"${aws_s3_bucket.example1.arn}"}
+        }
+    }]
+}
+POLICY
+}
 
 # S3 bucket to store App
 resource "aws_s3_bucket" "example1" {
@@ -20,6 +28,16 @@ resource "aws_s3_bucket" "example1" {
    tags = {
     Name        = "rawdata-bucket"
     Environment = "Dev"
+  }
+}
+#To create a S3 Bucket Notofication 
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket      = aws_s3_bucket.example1.id
+  eventbridge = true
+  topic {
+    topic_arn     = aws_sns_topic.topic.arn
+    events        = ["s3:*"]    
+    filter_suffix = ".png"
   }
 }
 
@@ -50,7 +68,3 @@ resource "aws_s3_bucket_public_access_block" "public_access" {
   restrict_public_buckets = true
 }
 
-resource "aws_s3_bucket_notification" "bucket_notification" {
-  bucket      = aws_s3_bucket.example1.id
-  eventbridge = true
-}
