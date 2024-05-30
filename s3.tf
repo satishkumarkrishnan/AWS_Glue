0d0af43e1d165d3b09e5ebdaaa6dc595231cefc6
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-30 13:13:36 +0530

    Adding AWS SNS Topic to send notification

--- s3.tf
+++ s3.tf
@@ -36,8 +36,8 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   eventbridge = true
   topic {
     topic_arn     = aws_sns_topic.topic.arn
-    events        = ["s3:*"]    
-    filter_suffix = ".png"
+    events        = ["s3:ObjectCreated:*"]    
+    filter_suffix = "*.*"
   }
 }
 
