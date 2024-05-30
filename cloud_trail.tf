5c004bb77db51a881017cd479993a62244dc4ae7
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-30 19:03:53 +0530

    Adding AWS cloud Trail resource to log S3 events

--- cloud_trail.tf
+++ cloud_trail.tf
@@ -1,24 +1,23 @@
-/*resource "aws_cloudtrail" "example" {
-  name = "tokyo_cloudtrail1"
+resource "aws_cloudtrail" "example" {
+  name = "tokyo_cloud_trail"
   s3_bucket_name = aws_s3_bucket.example1.id
 
   event_selector {
     read_write_type           = "All"
-    include_management_events = true
+    include_management_events = false
 
     data_resource {
       type   = "AWS::S3::Object"
       values = ["arn:aws:s3:::"]
     }
   }
-}*/
+}
 
 
-resource "aws_cloudtrail" "trail" {
+/*resource "aws_cloudtrail" "trail" {
   name                       = "tokyo_cloud_trail"
   cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail-cloudwatch-events-role.arn
-  #cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.loggroup.arn}:*"
- cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.tokyo_log_group.arn}:*"
+  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.tokyo_log_group.arn}:*"
   enable_log_file_validation = "false"
   enable_logging             = "true"
   is_multi_region_trail      = "false"
@@ -33,4 +32,4 @@ resource "aws_cloudtrail" "trail" {
       values = ["arn:aws:s3:::"]
     }
   }
-}
+}*/
