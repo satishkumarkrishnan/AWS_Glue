8e08b09c27143b18500313fb6a5b91431650d924
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-30 11:08:01 +0530

    Adding IAM policies

--- eventbridge.tf
+++ eventbridge.tf
@@ -1,4 +1,4 @@
-/*resource "aws_cloudwatch_event_rule" "event_from_s3" {
+resource "aws_cloudwatch_event_rule" "event_from_s3" {
   name = "s3_put_object_event"
   description = "Capture s3 object creation event"
   
@@ -14,4 +14,4 @@
         }
     }
   )
-}*/
+}
