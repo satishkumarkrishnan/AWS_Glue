88ab597bf5c71f2168dc161a3bb25a7f67419877
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-05-31 13:35:32 +0530

    Adding data policies to Bucket

--- data.tf
+++ data.tf
@@ -57,11 +57,11 @@ data "aws_iam_policy_document" "example1" {
 
     actions   = ["s3:GetBucketAcl"]
     resources = [aws_s3_bucket.example1.arn]
-    condition {
+    /*condition {
       test     = "StringEquals"
       variable = "aws:SourceArn"
       values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
-    }
+    }*/
   }
 
   statement {
@@ -81,10 +81,10 @@ data "aws_iam_policy_document" "example1" {
       variable = "s3:x-amz-acl"
       values   = ["bucket-owner-full-control"]
     }
-    condition {
+    /*condition {
       test     = "StringEquals"
       variable = "aws:SourceArn"
       values   = ["arn:${data.aws_partition.current.partition}:cloudtrail:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:trail/example"]
-    }
+    }*/
   }
 }
