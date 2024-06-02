01623b0118e72b4363d7721aaea9a7d270adba83
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-02 11:44:29 +0530

    cloudtrail data resource updated for S3 bucket

--- cloud_trail.tf
+++ cloud_trail.tf
@@ -23,23 +23,25 @@ resource "aws_cloudwatch_log_group" "cloudtrail_log_group" {
 resource "aws_cloudtrail" "trail" {
   name                       = "tokyo_cloudtrail"
   depends_on                 = [aws_s3_bucket_policy.logs, aws_iam_role.cloudtrail_cloudwatch_events_role] 
-  cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_cloudwatch_events_role.arn
+  #cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_cloudwatch_events_role.arn
+  cloud_watch_logs_group_arn = aws_cloudwatch_log_group.cloudtrail_log_group.arn
   #cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudwatch_log_group.arn}:*" 
-  cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail_log_group.arn}:*" # CloudTrail requires the Log Stream wildcard
+  #cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail_log_group.arn}:*" # CloudTrail requires the Log Stream wildcard
   enable_log_file_validation = "false"
   enable_logging             = "true"
   is_multi_region_trail      = "false"
  #kms_key_id                 = aws_kms_key.cloudtrail_logs_kms_key.arn
   s3_bucket_name             = aws_s3_bucket.example1.id
-   
+  include_global_service_events = false 
   event_selector {
     read_write_type           = "All"
     include_management_events = true
 
     data_resource {
       type   = "AWS::S3::Object"
-      values = ["arn:aws:s3"]
+     # values = ["arn:aws:s3"]
       #values = ["${aws_s3_bucket.example1.arn}/"]
+      values = ["arn:aws:s3:::${aws_s3_bucket.example1.id}/"]
     }
   }
 }
