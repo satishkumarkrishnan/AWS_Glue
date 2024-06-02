f85bb69d0dcf0310dc8297d13bf1a88e94ec028e
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-02 11:37:06 +0530

    cloudtrail data resource updated for S3 bucket

--- cloud_trail.tf
+++ cloud_trail.tf
@@ -22,7 +22,7 @@ resource "aws_cloudwatch_log_group" "cloudtrail_log_group" {
 # Resource to create Cloudtrail
 resource "aws_cloudtrail" "trail" {
   name                       = "tokyo_cloudtrail"
-  depends_on                 = [aws_s3_bucket_policy.logs] 
+  depends_on                 = [aws_s3_bucket_policy.logs, aws_iam_role.cloudtrail_cloudwatch_events_role] 
   cloud_watch_logs_role_arn  = aws_iam_role.cloudtrail_cloudwatch_events_role.arn
   #cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudwatch_log_group.arn}:*" 
   cloud_watch_logs_group_arn = "${aws_cloudwatch_log_group.cloudtrail_log_group.arn}:*" # CloudTrail requires the Log Stream wildcard
@@ -33,13 +33,13 @@ resource "aws_cloudtrail" "trail" {
   s3_bucket_name             = aws_s3_bucket.example1.id
    
   event_selector {
-    read_write_type           = "WriteOnly"
-    include_management_events = false
+    read_write_type           = "All"
+    include_management_events = true
 
     data_resource {
       type   = "AWS::S3::Object"
-      #values = ["arn:aws:s3:::${aws_s3_bucket.example1.id}/"]
-      values = ["${aws_s3_bucket.example1.arn}/"]
+      values = ["arn:aws:s3"]
+      #values = ["${aws_s3_bucket.example1.arn}/"]
     }
   }
 }
