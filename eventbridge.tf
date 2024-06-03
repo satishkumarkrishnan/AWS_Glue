5ad3ee98c97d5910e984b488f1d90935e063cc9b
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-03 10:52:28 +0530

    updating the AWS Eventbridge event rule and log group

--- eventbridge.tf
+++ eventbridge.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "event_from_s3" {
   name = "s3_put_object_event"
   description = "Capture s3 object creation event"
-  event_pattern = jsonencode(
+  /*event_pattern = jsonencode(
     {
         "source": ["aws.s3"],
         "detail-type": ["Object Created"],
@@ -11,7 +11,12 @@ resource "aws_cloudwatch_event_rule" "event_from_s3" {
             }
         }
     }
-  )   
+  )*/
+    event_pattern = jsonencode({
+    detail-type = [
+      "AWS Console Sign In via CloudTrail"
+    ]
+  })
 }
 
 #Resource creation for AWS Cloud Watch event target to store the events in target cloudwatch
