c87598a40cd024b50e17477f810fc3df60a47634
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-03 13:03:12 +0530

    updating the policy documnet for Eventbridge

--- data.tf
+++ data.tf
@@ -79,7 +79,7 @@ data "aws_iam_policy_document" "test" {
     }
     actions = ["events:PutEvents",]
     resources = [
-      "arn:aws:events:::${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:event-bus/default",
+    "arn:aws:events:::${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:event-bus/default",
     ]    
   }
 }
