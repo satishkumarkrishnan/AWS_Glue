3ddc41b0cee2debb1087a271fd2b399bf645b0ff
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-06 11:29:38 +0530

    Latest tf code for sfn resource

--- aws_step_function.tf
+++ aws_step_function.tf
@@ -1,7 +1,7 @@
 // Create state machine for step function
 resource "aws_sfn_state_machine" "sfn_state_machine" {
   name     = "sample-state-machine"
-  role_arn = "${aws_iam_role.iam_for_sfn.arn}"
+  role_arn = aws_iam_role.iam_for_sfn.arn
     definition = <<EOF
 {
   "Comment": "To trigger events from s3 to stepfunction to AWS Glue",
