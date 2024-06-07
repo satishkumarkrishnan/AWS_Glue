5cf071250dd976191cf743ee1ffb284658261b6f
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-07 15:44:56 +0530

    adding a new py script

--- input_dir/example.py
+++ input_dir/example.py
@@ -1,5 +1,13 @@
 import json
 import csv
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+glueContext = GlueContext(SparkContext.getOrCreate())
 
 # Script generated for node Amazon S3
 AmazonS3_node1698837830668 = glueContext.create_dynamic_frame.from_options(
