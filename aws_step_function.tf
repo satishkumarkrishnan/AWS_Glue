248a86182a59ce71b03038804dd4a3d4f144bd1e
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-11 06:05:13 +0530

    Updated SFN resource with type as standard

--- aws_step_function.tf
+++ aws_step_function.tf
@@ -3,8 +3,8 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
   name     = "sample-state-machine" 
   
   role_arn = aws_iam_role.iam_for_sfn.arn
-  type     = "EXPRESS"
-    definition = <<EOF
+  type     = "STANDARD"
+  definition = <<EOF
 {
   "Comment": "To trigger events from s3 to stepfunction to AWS Glue",
   "StartAt": "GlueJob",
