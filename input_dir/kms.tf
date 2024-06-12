66dc0c0141cf995a557bae9a74cd07cf1650a3b6
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-12 15:00:05 +0530

    Added the KMS resource

--- input_dir/kms.tf
+++ input_dir/kms.tf
@@ -1,13 +1,13 @@
 #To create KMS resource 
 resource "aws_kms_key" "ddsl_kms_key" {
-  description             = "KMS key for DDSL"
-  deletion_window_in_days = 10
-  key_usage               = "ENCRYPT_DECRYPT"
+  description             = "Data Domain AWS Key"
   enable_key_rotation     = true
+  key_usage               = "ENCRYPT_DECRYPT"
   tags = {
     Name = "ddsl_kms_key"    
   }
 }
+
 #To create KMS Alias
 resource "aws_kms_alias" "ddsl_kms_key_alias" {
   name          = "alias/kms_key"
