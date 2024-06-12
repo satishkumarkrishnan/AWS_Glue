389d4eb96a9d188329f6d9bb012ef34fdb1d7bf8
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-12 14:17:20 +0530

    Added the KMS resource

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
 #To create KMS Policy 
 resource "aws_kms_key_policy" "ddsl_kms_key_policy" {
-  key_id = aws_kms_key.ddsl_kms_key.arn
+  key_id = aws_kms_key.ddsl_kms_key.id
   policy = jsonencode({
     "Version" = "2012-10-17"
     "Id" = "KMS policy"
--- input_dir/kms.tf
+++ input_dir/kms.tf
@@ -9,7 +9,7 @@ resource "aws_kms_key" "ddsl_kms_key" {
   }
 }
 #To create KMS Alias
-resource "aws_kms_alias" "tokyo_kms_key_alias" {
+resource "aws_kms_alias" "ddsl_kms_key_alias" {
   name          = "alias/kms_key"
   target_key_id = aws_kms_key.ddsl_kms_key.id
 }
