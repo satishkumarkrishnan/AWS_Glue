41c01efa7cd5ec06bd2aa96a1fe50e4f10341215
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-13 10:28:53 +0530

    added the kms actions to gluepolicy

--- kms.tf
+++ kms.tf
@@ -9,7 +9,7 @@ resource "aws_kms_key" "ddsl_kms" {
 }
 
 #To create KMS Alias
-resource "aws_kms_alias" "ddsl_kms" {
-  name          = "alias/ddsl_kms_alias"
+resource "aws_kms_alias" "ddsl_kms_key" {
+  name          = "alias/ddsl_kms_key_alias"
   target_key_id = aws_kms_key.ddsl_kms.id
 }
