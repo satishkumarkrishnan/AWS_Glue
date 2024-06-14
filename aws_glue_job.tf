cf952b2918f0c97c44ae9852898ff483008d4e01
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-14 21:12:02 +0530

    Adding the updated pyspark script

--- aws_glue_job.tf
+++ aws_glue_job.tf
@@ -6,7 +6,7 @@ resource "aws_glue_job" "example" {
   glue_version = "4.0"
   command {
     #name            = "pythonshell"
-    script_location = "s3://${aws_s3_bucket.example1.bucket}/pyspark.py"
+    script_location = "s3://${aws_s3_bucket.example1.bucket}/segregate.py"
     python_version = "3"
   }
    default_arguments = {    
@@ -29,7 +29,7 @@ resource "aws_glue_job" "data_lineage" {
     python_version = "3"
   }
    default_arguments = {    
-    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.glue_job_log_group.name
+    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.data_lineage_log_group.name
     "--enable-continuous-cloudwatch-log" = "true"
     "--enable-continuous-log-filter"     = "true"
     "--enable-metrics"                   = ""
