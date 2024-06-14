1436599bdc6a97d00fc5b80238ff9a68bafac6d1
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-14 09:02:53 +0530

    Adding pyspark script

--- input_dir/pyspark.py
+++ input_dir/pyspark.py
@@ -1,3 +1,24 @@
 from pyspark.sql import SparkSession
-spark = SparkSession.builder.appName('abc').getOrCreate()
-df=spark.read.csv('filename.csv',header=True)
+
+# Path to the OpenLineage Spark agent JAR file
+jar_path = "s3://ddsl-rawdata-bucket/openlineage-spark_2.13-1.13.1.jar"
+
+# Initialize the Spark session
+spark = SparkSession.builder \
+    .appName("OpenLineageExample") \
+    .config("spark.jars", jar_path) \
+    .config("spark.openlineage.transport.type", "http") \
+    .getOrCreate() \
+    .config("spark.extraListeners", "io.openlineage.spark.agent.OpenLineageSparkListener") \
+    .config("spark.openlineage.transport.type", "http") \
+    .config("spark.openlineage.transport.url", "http://localhost:5000") \
+    .config("spark.openlineage.namespace", "spark_namespace") \
+    .config("spark.openlineage.parentJobNamespace", "airflow_namespace") \
+    .config("spark.openlineage.parentJobName", "airflow_dag.airflow_task") \
+    .config("spark.openlineage.parentRunId", "xxxx-xxxx-xxxx-xxxx") \
+
+
+# Your Spark job code
+# ...
+
+spark.stop()
