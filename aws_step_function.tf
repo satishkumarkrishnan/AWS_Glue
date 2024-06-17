29ef29d6231fe1de28bbf097946887441b47380e
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-17 18:33:39 +0530

    added Data lineage job

--- aws_step_function.tf
+++ aws_step_function.tf
@@ -21,7 +21,7 @@ resource "aws_sfn_state_machine" "sfn_state_machine" {
                     "Type": "Task",
                     "Resource": "arn:aws:states:::glue:startJobRun.sync",
                     "Parameters": {
-                        "JobName": "${aws_glue_job.example.name}"
+                        "JobName": "${aws_glue_job.data_lineage.name}"
                     },
                     "End": true
                 }
