8ad995225e94b6f54d2fb2f642a09949d9d11ef7
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-06-18 15:50:11 +0530

    Data lineage

--- aws_glue_crawler.tf
+++ aws_glue_crawler.tf
@@ -6,7 +6,7 @@
 
 }*/
 
-resource "aws_glue_crawler" "example" {
+/*resource "aws_glue_crawler" "example" {
   #database_name = "${aws_glue_catalog_database.example.name}"
   database_name = var.database_name
   name          = "tokyo_crawler"
@@ -15,4 +15,4 @@ resource "aws_glue_crawler" "example" {
   s3_target {
     path = "s3://${aws_s3_bucket.example1.id}"
   }  
-}
+}*/
