458aaee4fe71e2325a83d589ddc610599f30994e
satishkumarkrishnan <satishkumar.krishnan83.com>
2024-07-01 15:44:23 +0530

    updated lineage code

--- aws_glue_job.tf
+++ aws_glue_job.tf
@@ -55,3 +55,22 @@ resource "aws_glue_job" "data_quality2" {
     "--enable-metrics"                   = ""
   }
 }
+
+#AWS Glue job for a Py script
+resource "aws_glue_job" "data_lineage" {
+  name = "DDSL_Datalineage_job"
+  role_arn = aws_iam_role.gluerole.arn
+  max_capacity = "1.0"
+  glue_version = "4.0"
+  command {
+    #name            = "pythonshell"
+    script_location = "s3://${aws_s3_bucket.example1.bucket}/linage.py"
+    python_version = "3"
+  }
+   default_arguments = {    
+    "--continuous-log-logGroup"          = aws_cloudwatch_log_group.data_quality_datalineage.name
+    "--enable-continuous-cloudwatch-log" = "true"
+    "--enable-continuous-log-filter"     = "true"
+    "--enable-metrics"                   = ""
+  }
+}
